6792bca7ff1db5243265afd9f8d453d23153848f
Walee Sebiiy <waleesebiiy@gmail.com>
2024-02-18 12:35:59 +0100

    init.

--- api/src/test/java/studio/farsighted/pfe/api/ApiApplicationTests.java
+++ api/src/test/java/studio/farsighted/pfe/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.pipe;
+package studio.farsighted.pfe.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PipeApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {

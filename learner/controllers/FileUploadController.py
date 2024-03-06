09ed1c72d3c5ffff1c65b6df3bd7c07a05b34fc6
Walee Sebiiy <waleesebiiy@gmail.com>
2024-03-06 12:26:54 +0100

    update the upload function

--- learner/controllers/FileUploadController.py
+++ learner/controllers/FileUploadController.py
@@ -11,10 +11,21 @@ class FileUploadController:
         if not os.path.exists("upload"):
                 os.makedirs("upload")
 
-    @endpoint.route('', methods=['POST'])
-    def upload():
-        file = request.files['file']
+@endpoint.route('', methods=['POST'])
+def upload():
+    file = request.files['file']
+    allowed_extensions = {'xls', 'xlsx', 'csv'}
+
+    # Extract the file extension and check if it's allowed
+    file_extension = file.filename.rsplit('.', 1)[1].lower()
+    if file_extension in allowed_extensions:
         if os.getenv("APP_STORAGE") == "local":
-            file.save(os.path.join("upload", file.filename))
-            return jsonify({"timestamp": time.time(), "status": 204, 'message': 'File uploaded successfully'}), 200
-        return jsonify({"timestamp": time.time(), "status": 401, 'message': 'File uploaded failed'}), 401
+            file.save(os.path.join("upload", f"{file.filename.rsplit('.', 1)[0].lower()}-{time.time()}.{file_extension}"))
+            return jsonify({"timestamp": time.time(), "success": "Upload Successfully", "status": 204, 'message': 'File uploaded successfully'}), 200
+        else:
+            # Handle non-local storage logic if applicable
+            pass
+    else:
+        return jsonify({"timestamp": time.time(), "error" : "Upload Error", "status": 400, 'message': 'File format not supported. Please upload xls, xlsx, or csv files only.'}), 400
+    # If APP_STORAGE is not set to "local" and no file is processed
+    return jsonify({"timestamp": time.time(), "error" : "Upload Error", "status": 401, 'message': 'File upload failed'}), 401

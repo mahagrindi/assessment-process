a05b4f22f3340c05723d054c769ccd256bebe0f6
Walee Sebiiy <waleesebiiy@gmail.com>
2024-03-29 11:39:52 +0100

    adding preline plugin to tailwind

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,10 +3,10 @@ import { ey } from './styles/theme/tailwind-custom-color'
 
 const config: Config = {
   darkMode: 'class',
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', 'node_modules/preline/dist/*.ts'],
   theme: {
     colors: ey,
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('preline/plugin')],
 }
 export default config

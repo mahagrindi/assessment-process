6f08fe60b28d59a183feafb7f19e06873af0e18b
Walee Sebiiy <waleesebiiy@gmail.com>
2024-03-31 11:38:40 +0100

    tailwind custom themeing

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from 'tailwindcss'
-import { ey } from './styles/theme/tailwind-custom-color'
+import { EY_COLORS } from './styles/theme/tailwind-custom-color'
 
 const config: Config = {
   darkMode: 'class',
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', 'node_modules/preline/dist/*.ts'],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './templates/**/*.{js,ts,jsx,tsx,mdx}', 'node_modules/preline/dist/*.ts'],
   theme: {
-    colors: ey,
+    colors: { transparent: 'transparent', current: 'currentColor', ...EY_COLORS },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('preline/plugin')],
 }
